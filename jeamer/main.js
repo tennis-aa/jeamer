86a49675d15f35f829814e233ea3b7cb34261bba
tennis-aa <andresalban94@gmail.com>
2024-06-19 00:48:22 +0200

    commander cli

--- jeamer/main.js
+++ jeamer/main.js
@@ -1,16 +1,40 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
+import { Command } from 'commander';
 import render from './render.js';
+import { beamerToRevealjs } from './beamerToJeamer.js';
 
-const ifile = process.argv[2]
-let ofile;
+const program = new Command();
 
-if (process.argv.length === 4) {
-    ofile = process.argv[3];
-} else {
-    const fileParts = ifile.split(".");
-    fileParts[fileParts.length - 1] = "html";
-    ofile = fileParts.join(".");
-}
+program.name("jeamer")
+  .description("Create html slides from Nunjucks templates")
+  .argument("<input>", "Input file")
+  .option("-o, --output <output>", "Output file")
+  .option("-p, --pretty", "Pretty print")
+  .action((ifile,options) => {
+    if (options.output === undefined) {
+      let fileParts = ifile.split(".");
+      fileParts[fileParts.length - 1] = "html";
+      options.output = fileParts.join("."); 
+    }
+    render(ifile,options.output, options.pretty ? "pretty": "unformatted");
+  });
 
-render(ifile, ofile, "pretty");
+program.command("frombeamer")
+  .description(`Convert a beamer (latex) source file into a template to be used with jeamer. 
+This utility is not robust and will likely require several manual adjustments after conversion.`)
+  .argument("<input>", "Input file")
+  .option("-o, --output <output>", "Output file")
+  .action((ifile,options) => {
+    if (options.output === undefined) {
+      let fileParts = ifile.split(".");
+      fileParts[fileParts.length - 1] = "njk";
+      options.output = fileParts.join("."); 
+    }
+    const tex = fs.readFileSync(ifile,"utf-8");
+    const j = beamerToRevealjs(tex);
+    fs.writeFileSync(options.output, j, "utf-8");
+  });
+
+program.parse();
